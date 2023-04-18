05794bb81f456efebb0c5b067d0098ecc84f3eaf
Yonatan Tesfaye <yonatantesfaye@pop-os.localdomain>
2023-04-18 18:59:02 +0300

    signup page

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Signup from './pages/signup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="text-red-500"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Signup />
     </div>
   );
 }
