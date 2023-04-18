65355e9b1f2c708c54259fd819b4dcce1260f55b
Natnael Yeshiwas <natiyeshi16@gmail.com>
2023-04-18 14:10:03 +0300

    mongoose added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const cors = require('cors')
 const {connect_db} = require('./config/db')
+const color = require("colors")
+
 require('dotenv').config()
 
 const app = express()
